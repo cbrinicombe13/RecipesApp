0aef33f6772ba66e1cc2d3a1f519c6e8e4260211
cbrinicombe13 <cbrinicombe13@googlemail.com>
2020-02-04 19:09:36 +0000

    Favorites tab added with dummy data, not favorites yet

--- screens/FavoritesScreen.js
+++ screens/FavoritesScreen.js
@@ -1,12 +1,40 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, FlatList } from 'react-native'
+
+import RecipeTile from '../components/RecipeTile';
+import { RECIPES } from '../data/data';
 
 const FavoritesScreen = (props) => {
+    const favs = RECIPES.filter(recipe => recipe.categoryIds.indexOf('c2') >= 0);
+
+    const renderRecipe = (itemData) => {
+        const { imageUrl, title, duration, complexity, affordability } = itemData.item;
+        return (
+            <RecipeTile
+                // onPress={() => {
+                //     props.navigation.navigate({
+                //         routeName: 'Recipe',
+                //         params: {
+                //             recipe: itemData.item
+                //         }
+                //     });
+                // }}
+                onPress={() => {}}
+                navigation={props.navigation}
+                image={imageUrl}
+                title={title}
+                duration={duration}
+                complexity={complexity}
+                affordability={affordability}
+                recipe={itemData.item}
+            />
+        );
+    }
+
+
     return (
-        <View>
-            <Text>Favorites Screen</Text>
-        </View>
-    )
+        <FlatList data={favs} renderItem={renderRecipe} numColumns={1} />
+    );
 }
 
 export default FavoritesScreen
